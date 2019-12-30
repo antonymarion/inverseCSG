2004d498ba07a684e5d0d0790f7a85f2c7c94775
antonymarion <marionantony@gmail.com>
2019-12-30 22:37:11 +0100

    Update helper.py

--- helper.py
+++ helper.py
@@ -33,6 +33,19 @@ def Run(command, exception_handle=DefaultExceptionHandle):
     exception_handle(command, exit_code)
   return exit_code
 
+def RunWithStdout(command, exception_handle=DefaultExceptionHandle):
+  print('Running the following command:')
+  PrintWithGreenColor(command)
+  time_start = time.time()
+  ret_val = os.system(command)
+  time_intvl = time.time() - time_start
+  print('Command finished in %f seconds.' % time_intvl)
+  exit_code = ret_val >> 8
+  if exit_code != 0 and exception_handle is not None:
+    os.system('ls')
+    exception_handle(command, exit_code)
+  return exit_code
+
 ################################################################################
 # Triangle meshes.
 ################################################################################
